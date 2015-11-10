555fc799e89f6d13dce4f2e63b069578170d4627
Jim Witschey <jim.witschey@gmail.com>
2015-11-10 18:32:54 -0500

    clean up tests

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ import unittest
 import logging
 from mock import MagicMock
 from nose.plugins import PluginTester
-from nose.tools import assert_false, assert_true
+from nose.tools import assert_false, assert_true, assert_is_not, assert_is
 
 from nose_call_on_hang.nose_call_on_hang import CallOnHang
 
@@ -30,13 +30,13 @@ def _fresh_plugins_with_mock(timeout):
     return [plugin_under_test], call_on_hang_mock
 
 
-class _CallOnHangTestException(RuntimeError):
-    pass
-
-
 class _TestCallOnHangMixin(PluginTester, unittest.TestCase):
     timeout, wait = None, None
     activate = '--with-callonhang'
+
+    def setUp(self):
+        self.call_on_hang_mock.reset_mock()
+        super(_TestCallOnHangMixin, self).setUp()
 
     def makeSuite(self):
         wait = self.wait
@@ -45,20 +45,26 @@ class _TestCallOnHangMixin(PluginTester, unittest.TestCase):
             def runTest(self):
                 log.debug('sys.stdout is the initial value: {}'.format(
                     sys.stdout is _cached_stdout))
-                log.debug('sleeping {} seconds'.format(wait))
+                assert_is_not(sys.stdout, _cached_stdout)  # sys.stdout should be replaced
 
+                log.debug('sleeping {} seconds'.format(wait))
                 time.sleep(wait)
 
         return [TestHang()]
 
     def test_sleep(self):
         log.debug(self.call_on_hang_mock)
-        assert_true(self.nose.success)
+        assert_true(self.nose.success)  # all the tests should pass
+        assert_true(self.output)  # output shouldn't be empty, i.e. redirection should still be working
+        assert_is(sys.stdout, _cached_stdout)  # sys.stdout should be restored by now
 
         if self.wait > self.timeout:
-            assert_true(self.call_on_hang_mock.called)
+            # if the background timer's timeout is longer than how long the test will wait,
+            # the mock should have been called
+            assert_false(self.call_on_hang_mock.called)
             log.debug('it was called!')
         else:
+            # otherwise, it shouldn't have been called
             assert_false(self.call_on_hang_mock.called)
             log.debug('not called!')
 
@@ -77,6 +83,7 @@ class TestCallSleep2SecondsTwice(_TestCallOnHangMixin):
     timeout, wait = 5, 2
     plugins, call_on_hang_mock = _fresh_plugins_with_mock(timeout)
     test_sleep_again = _TestCallOnHangMixin.test_sleep
+
 
 class TestCallSleep7SecondsTwice(_TestCallOnHangMixin):
     timeout, wait = 5, 7
